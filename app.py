17a4afb9a1d7b266594baee4012829dc49fd6f65
zss1397 <zss17@mail.aub.edu>
2025-09-08 22:56:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -127,16 +127,16 @@ with col1:
     fig1 = px.pie(size_dist, values='Count', names='Institution Size', hole=0.5,
                   color_discrete_sequence=['#FF6B6B', '#4ECDC4', '#45B7D1'])
     fig1.update_traces(
-        textposition='inside', 
+        textposition='auto', 
         textinfo='percent+value',
-        textfont_size=16,
+        textfont_size=14,
         textfont_color='white',
         texttemplate='%{percent}<br>%{value:,}'
     )
     fig1.update_layout(
-        height=220,
+        height=250,
         template='plotly_white',
-        margin=dict(l=20, r=20, t=10, b=20),
+        margin=dict(l=40, r=40, t=10, b=20),
         annotations=[dict(text=f'Total<br>{size_dist["Count"].sum():,}', x=0.5, y=0.5, font_size=16, showarrow=False)]
     )
     st.plotly_chart(fig1, use_container_width=True)
@@ -147,16 +147,16 @@ with col2:
     fig2 = px.pie(sector_data, values='Total Count', names='Sector',
                   color_discrete_sequence=['#FFD700', '#4169E1', '#8A2BE2'])
     fig2.update_traces(
-        textposition='inside', 
+        textposition='auto', 
         textinfo='percent+value',
-        textfont_size=16,
+        textfont_size=14,
         textfont_color='white',
         texttemplate='%{percent}<br>%{value:,}'
     )
     fig2.update_layout(
-        height=220,
+        height=250,
         template='plotly_white',
-        margin=dict(l=20, r=20, t=10, b=20)
+        margin=dict(l=40, r=40, t=10, b=20)
     )
     st.plotly_chart(fig2, use_container_width=True)
 
